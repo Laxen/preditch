c6ac6a7ebcc08c358d4bf8281e1ba816e3d1fe6a
Alexander Ganslandt <alex.ganslandt@gmail.com>
2025-05-11 14:31:32 +0200

    Add chat support n shit

--- preditch.js
+++ preditch.js
@@ -10,6 +10,7 @@ let client = null;
 let targetWord = '';
 let pointCount = 0;
 let sseClients = [];
+let chatClients = [];
 
 app.post('/start', (req, res) => {
   const { channel, word } = req.body;
@@ -22,6 +23,7 @@ app.post('/start', (req, res) => {
 
   client.on('message', (_, tags, message) => {
     console.log(message);
+    chatClients.forEach(res => res.write(`data: ${tags['display-name']}: ${message}\n\n`));
     if (message.toLowerCase().includes(targetWord)) {
       pointCount++;
       sseClients.forEach(res => res.write(`data: ${pointCount}\n\n`));
@@ -41,6 +43,18 @@ app.get('/points', (req, res) => {
 
   req.on('close', () => {
     sseClients = sseClients.filter(c => c !== res);
+  });
+});
+
+app.get('/chat', (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+  res.flushHeaders();
+  chatClients.push(res);
+
+  req.on('close', () => {
+    chatClients = chatClients.filter(c => c !== res);
   });
 });
 
